ee9dfb7ca0b5bcf18a8e46cb3e73616f0383b05c
Gabriel Souza <gabrie.ssouza@gmail.com>
2019-06-21 01:31:22 -0300

    fix(env): fix dotenv safe

--- packages/shared/src/config/index.ts
+++ packages/shared/src/config/index.ts
@@ -12,5 +12,3 @@ export const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/house-aut
 export const SERVER_PORT = process.env.PORT || 3000
 export const NODE_ENV = process.env.NODE_ENV
 export const JWT_SECRET = process.env.JWT_SECRET
-export const SERIAL_PORT = process.env.SERIAL_PORT
-export const GRAPHQL_URL = process.env.GRAPHQL_URL
