1f23afbf4a7114b84a28ecfadb4a5ef94fb38b76
Gabriel Souza <gabrie.ssouza@gmail.com>
2019-06-21 00:29:42 -0300

    fix(procfile): fix comand

--- packages/web/server.js
+++ packages/web/server.js
@@ -2,13 +2,13 @@ require('@babel/polyfill');
 const express = require('express')
 const path = require('path')
 
-const app = express()
+const server = express()
 const PORT = process.env.PORT || 5000
 
-app.use(express.static(path.join(__dirname, 'build')))
+server.use(express.static(path.join(__dirname, 'build')))
 
-app.get('*', function(req, res) {
+server.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-app.listen(PORT, () => console.log(`Listening on :${PORT}`))
+server.listen(PORT, () => console.log(`Listening on :${PORT}`))
