8decf926e67c6530de1b89ea68f38098c088b887
Gabriel Souza <gabrie.ssouza@gmail.com>
2019-06-21 00:07:23 -0300

    fix(heroku): fix package json

--- packages/web/server.js
+++ packages/web/server.js
@@ -5,10 +5,10 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'))
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 app.listen(PORT, () => console.log(`Listening on :${PORT}`))
