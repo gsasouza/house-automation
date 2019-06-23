a420da97652eaf2474c65875d10a82820e59ad85
Gabriel Souza <gabrie.ssouza@gmail.com>
2019-06-22 23:08:44 -0300

    feat(subscriptions): add subscriptions on client

---
+++ packages/web/src/components/rooms/subscriptions/BoardIoChangedSubscription.ts
@@ -0,0 +1,43 @@
+import {
+  graphql,
+  requestSubscription
+} from 'react-relay'
+import environment from '../relay/environment';
+
+const boardIoSubscription = graphql`
+  subscription BoardIoChangedSubscription{
+    BoardIoChanged {
+      boardIoEdge {
+        node {
+          state
+          id
+        }
+      }
+    }
+  }
+`
+
+export default () => {
+
+  const subscriptionConfig = {
+    subscription: boardIoSubscription,
+    variables: {},
+    updater: store => {
+      const rootField = store.getRootField('BoardIoChanged')
+      const boardIoEdge = rootField.getLinkedRecord('boardIoEdge')
+      const node = boardIoEdge.getLinkedRecord('node')
+      const nodeId = node.getValue('id')
+      const newState = node.getValue('state')
+
+      const nodeProxy = store.get(nodeId);
+      nodeProxy.setValue(newState, 'state');
+    },
+    onError: error => console.log(`An error occured:`, error)
+  }
+
+  requestSubscription(
+    environment,
+    subscriptionConfig
+  )
+
+}
