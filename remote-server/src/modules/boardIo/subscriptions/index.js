63c748168995542f4d06b2d086427dab6ac8cb8a
Gabriel Souza <gabrie.ssouza@gmail.com>
2023-05-08 14:53:28 -0300

    cleanup build files

--- remote-server/src/modules/boardIo/subscriptions/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const BoardIoChangedSubscription_1 = __importDefault(require("./BoardIoChangedSubscription"));
-exports.default = {
-    BoardIoChanged: BoardIoChangedSubscription_1.default,
-};
